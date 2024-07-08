77f4a2cc8c8dc5771329141b8b4bcdb0f491a91c
anthony-mini <anthony-mini@outlook.fr>
2024-07-08 22:06:29 +0200

    chore: Update project configuration and dependencies.
    Add entry point for React Application
    Install prettier and React dependencies tools.

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+// Composant principal de l'application React
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is the end...</p>
+    </div>
+  );
+};
+
+export default App;

40dcd4d78b84adf9191150e3f408b0eac90146a3
anthony-mini <anthony-mini@outlook.fr>
2024-07-09 23:47:38 +0200

    wip: socket.io & http client

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,10 @@
-// Point d'entrée de l'API Express
-
 import pkg from 'pg';
 const { Client } = pkg;
 import express from 'express';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
 async function main() {
-  const app = express();
-  app.use(express.json());
-
   const client = new Client({
     user: 'myusername',
     host: 'localhost',
@@ -21,7 +18,83 @@ async function main() {
     console.log('Database connection successful');
   } catch (error) {
     console.error('Error during database connection', error);
+    return;
   }
+
+  // Initialize Express
+  const app = express();
+
+  // Middleware pour parser les requêtes en JSON
+  app.use(express.json());
+
+  // Cors middleware pour autoriser toutes les requêtes
+  app.use(
+    cors({
+      origin: '*',
+    }),
+  );
+
+  // ROUTES
+
+  // GET /users
+  app.get('/users', async (req, res) => {
+    try {
+      const result = await client.query('SELECT * FROM users');
+      res.json(result.rows);
+    } catch (error) {
+      console.error('Error retrieving users:', error);
+      res.status(500).json({ error: 'Database error' });
+    }
+  });
+
+  // Create HTTP Server and listen on port 3000
+  app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+  });
+
+  // Create Socket.io server
+  const io = new Server(3001, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  // Event listener for new connection
+  io.on('connection', (socket) => {
+    console.log('New connection', socket.id);
+
+    // Event listener for new message
+    socket.on('message', async (message) => {
+      console.log('message received', message);
+
+      try {
+        // Validate message
+        if (!message.content || !message.userId || !message.channelId) {
+          throw new Error('Invalid message format');
+        }
+
+        // Insert message into database
+        const result = await client.query(
+          'INSERT INTO messages (content, "userId", "channelId") VALUES ($1, $2, $3) RETURNING *',
+          [message.content, message.userId, message.channelId],
+        );
+        console.log('Message saved to database', result.rows[0]);
+
+        // Emit message to all clients
+        io.emit('message', result.rows[0]);
+      } catch (error) {
+        console.error('Error saving message to database', error);
+        socket.emit('error', 'Error saving message to database');
+      }
+    });
+
+    // Event listener for disconnection
+    socket.on('disconnect', () => {
+      console.log('Client disconnected', socket.id);
+    });
+  });
+
+  console.log('Socket.io server started on http://localhost:3001');
 }
 
 main();
